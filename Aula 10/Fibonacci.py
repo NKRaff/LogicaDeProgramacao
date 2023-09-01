522b210962b92a7d669c418cee1dde2e036a09fe
NKRaff <rafaelgus3000@gmail.com>
2023-09-01 16:54:55 -0300

    Adição final da Aula 10

--- Aula 10/Fibonacci.py
+++ Aula 10/Fibonacci.py
@@ -1,8 +1,21 @@
+# COPIADO
+
 import os
 os.system('cls')
 
-num = int(input("Digite um numero: "))
-anterior = 1
+num = int(input("Que termo deseja encontrar: "))
+ultimo=1
+penultimo=1
 
-for i in range(1, num):
-    print()
+if (num==1):
+    print("0, 1")
+elif (num == 2):
+    print("0, 1, 2")
+else:
+    print("0, 1", end="")
+    for i in range(2,num,2):
+        termo = ultimo + penultimo
+        penultimo = ultimo
+        ultimo = termo
+        print(f", {termo}",end="")
+        
