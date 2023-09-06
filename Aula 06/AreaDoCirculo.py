e24cda9441a5c3697d3197e42fb747ae8f503f5a
Rafael Rocha <rafaelgus3000@gmail.com>
2023-09-06 10:46:42 -0300

    Delete Aula 06 directory

--- Aula 06/AreaDoCirculo.py
+++
@@ -1,8 +0,0 @@
-import os
-import math
-os.system('cls')
-
-PI = 3.14
-raio = float(input("Digite o Raio: "))
-
-print(f"Area: {(raio**2 * PI):.2f}")
--- Aula 06/AreaDoQuadrado.py
+++
@@ -1,6 +0,0 @@
-import os
-os.system('cls')
-
-num = float(input("Digite o valor do lado: "))
-
-print(f"Area: {num * num}")
